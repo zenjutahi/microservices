3827220570c90ac86e626ce351ab9ae207ec5851
zenjutahi <zenjutahi@gmail.com>
2022-03-14 15:30:17 +0300

    create controller and class/entity

---
+++ currency-exchange-service/src/main/java/com/microservice/currency/exchange/service/currencyexchangeservice/CurrencyExchange.java
@@ -0,0 +1,52 @@
+package com.microservice.currency.exchange.service.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+public class CurrencyExchange {
+    private long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+
+    public CurrencyExchange(long id, String from, String to, BigDecimal conversionMultiple) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+    }
+
+    public CurrencyExchange() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public BigDecimal getConversionMultiple() {
+        return conversionMultiple;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public void setConversionMultiple(BigDecimal conversionMultiple) {
+        this.conversionMultiple = conversionMultiple;
+    }
+}
