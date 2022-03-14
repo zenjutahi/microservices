70c201b9746ca5b4f41337497829dfc0751e76ca
zenjutahi <zenjutahi@gmail.com>
2022-03-14 16:39:38 +0300

    converty currency exchange to an entity and populate with data

--- currency-exchange-service/src/main/java/com/microservice/currency/exchange/service/currencyexchangeservice/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/microservice/currency/exchange/service/currencyexchangeservice/CurrencyExchange.java
@@ -1,10 +1,23 @@
 package com.microservice.currency.exchange.service.currencyexchangeservice;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
+
+@Entity
 public class CurrencyExchange {
+
+    @Id
+    @GeneratedValue
     private long id;
+
+    @Column(name="currency_from")
     private String from;
+
+    @Column(name="currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private String environment;
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment)
+values(10001,'USD','INR',65,'');
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment)
+values(10012,'EUR','KSH',265,'');
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment)
+values(10023,'AUD','INR',35,'');
