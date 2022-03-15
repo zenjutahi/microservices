62cd8844a1cb74dab126738909120a0556f5bd97
zenjutahi <zenjutahi@gmail.com>
2022-03-15 10:42:22 +0300

    rerouting with spring cloud api gateway

--- api-gateway/src/main/java/com/microservice/api/gateway/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/microservice/api/gateway/apigateway/ApiGatewayApplication.java
@@ -1,5 +1,6 @@
 package com.microservice.api.gateway.apigateway;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
