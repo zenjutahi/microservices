cdf3e42393287c36315aa3ca40fb4f2d5624fd49
zenjutahi <zenjutahi@gmail.com>
2022-03-15 11:10:04 +0300

    setting up circuit breaker with resillience4j

---
+++ currency-exchange-service/src/main/java/com/microservice/currency/exchange/service/currencyexchangeservice/CircuitBreakerController.java
@@ -0,0 +1,15 @@
+package com.microservice.currency.exchange.service.currencyexchangeservice;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CircuitBreakerController {
+
+    @GetMapping("/sample-api")
+    public String sampleApi(){
+        return  "Sample API";
+    }
+
+}
